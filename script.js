8319f978f30ffefa9d438980d31f0d48f9f90ebc
Glaucoed <gesverzuthe@gmail.com>
2022-07-04 19:54:20 -0300

    requisito 8 realizado

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 
 const localClassCor = document.getElementsByClassName('color');
+// const
 
 function alteraCor() {
   const possiveisCores = ['black', 'yellow', 'green', 'red'];
@@ -33,8 +35,19 @@ for (let index = 0; index < localClassCor.length; index += 1) {
 }
 
 function atribuiClass(event) {
-  const clSelected = document.querySelector('.selected');
-  clSelected.classList.remove('selected');
+  const selectionClass = document.querySelector('.selected');
+  selectionClass.classList.remove('selected');
   event.target.classList.add('selected');
-  
+}
+quadrados = document.getElementsByClassName('pixel');
+for (let index = 0; index < quadrados.length; index += 1) {
+  quadrados[index].addEventListener('click', adicionarCor);
+}
+function adicionarCor(event) {
+  const selectionClass = document.querySelector('.selected');
+  if (selectionClass.style.backgroundColor === event.target.style.color) {
+    event.target.style.backgroundColor = 'white';
+  } else {
+    event.target.style.backgroundColor = selectionClass.style.backgroundColor;
+  }
 }
